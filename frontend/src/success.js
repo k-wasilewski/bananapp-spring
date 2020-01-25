57e842049a6df15b1cf8341b66b8a604f38723a1
Kuba <k.k.wasilewski@gmail.com>
2020-01-25 12:58:59 +0100

    passing username to react

--- frontend/src/success.js
+++ frontend/src/success.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import './App.css';
+import axios from "axios";
 
 class Success extends React.Component {
 
+    state = {
+        username: "n/a"
+    }
+
     render() {
+        var $this = this;
+        axios.get('http://localhost:8081/username')
+            .then(function (response) {
+                $this.setState({username: response.data})
+            });
+
         return (
             <div className="App">
                 <header className="App-header">
-                    <h3> Logged-in, </h3>
+                    <h3> Logged-in as { this.state.username }</h3>
                     <Link to="/">
                         <button variant="outlined">
                             back
