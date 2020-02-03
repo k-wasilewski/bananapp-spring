924f77f4f648e8492774df3e565880031805a902
Kuba <k.k.wasilewski@gmail.com>
2020-02-03 09:56:27 +0100

    'filename exists' handling

--- frontend/src/results.js
+++ frontend/src/results.js
@@ -17,28 +17,44 @@ class Results extends React.Component {
         const accuracy = accRegex.exec(prediction);
 
         var days = '[error]';
-        if (score[1]==1.0) days="1 day";
-        else if (score[1]==2.0) days="2 days";
-        else if (score[1]==3.0) days="3 days";
-        else if (score[1]==4.0) days="4 days";
-        else if (score[1]==5.0) days="5 days";
-        else if (score[1]==6.0) days="6 days";
-        else if (score[1]==7.0) days="7 days";
 
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={img} style={{maxHeight: "500px"}} />
-                    <h3> Your banana is { days } old, </h3>
-                    <h3> with { Number((accuracy[1]*100).toFixed(2)) }% certainty</h3>
-                    <Link to="/">
-                        <button variant="outlined">
-                            back
-                        </button>
-                    </Link>
-                </header>
-            </div>
-        );
+        if (!score) {
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <h3>file already exists, change the filename and try again</h3>
+                        <Link to="/">
+                            <button variant="outlined">
+                                back
+                            </button>
+                        </Link>
+                    </header>
+                </div>
+            )
+        } else {
+            if (score[1]==1.0) days="1 day";
+            else if (score[1]==2.0) days="2 days";
+            else if (score[1]==3.0) days="3 days";
+            else if (score[1]==4.0) days="4 days";
+            else if (score[1]==5.0) days="5 days";
+            else if (score[1]==6.0) days="6 days";
+            else if (score[1]==7.0) days="7 days";
+
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <img src={img} style={{maxHeight: "500px"}} />
+                        <h3> Your banana is { days } old, </h3>
+                        <h3> with { Number((accuracy[1]*100).toFixed(2)) }% certainty</h3>
+                        <Link to="/">
+                            <button variant="outlined">
+                                back
+                            </button>
+                        </Link>
+                    </header>
+                </div>
+            )
+        }
     }
 }
 export default Results;
