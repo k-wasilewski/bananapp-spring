85d4db807c1cfc1412d7dee8c32ea830925b48f6
k-wasilewski <k.k.wasilewski@gmail.com>
2020-07-04 10:45:35 +0200

    SecurityConfigTest passed

---
+++ src/test/java/org/app/auth/security/SecurityConfigTest.java
@@ -0,0 +1,38 @@
+package org.app.auth.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SecurityConfigTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username = "test@test.pl", password = "test", roles = "USER")
+    public void authPathAccess() throws Exception {
+        mockMvc.perform(get("/auth/files"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        mockMvc.perform(get("/logout"))
+                .andExpect(status().isNoContent())
+                .andReturn();
+
+        mockMvc.perform(get("/auth/files"))
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+    }
+}
